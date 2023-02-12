1d0985aba1ed0919b6c33d0af818d59f289847ab
pomdtr <achille.lacoin@gmail.com>
2023-02-12 21:13:28 +0100

    temporarely remove the sharing feature

--- components/app.tsx
+++ components/app.tsx
@@ -3,15 +3,12 @@ import {
   Excalidraw,
   loadFromBlob,
   MainMenu,
-  WelcomeScreen,
   serializeAsJSON,
-  LiveCollaborationTrigger,
 } from "@excalidraw/excalidraw";
 
 import _ from "lodash";
 import { useEffect, useRef } from "react";
 import { useState } from "react";
-import { UsersIcon } from "./icons";
 
 declare global {
   interface Window {
@@ -21,7 +18,6 @@ declare global {
 
 export default function App(props: { drawing: Drawing; readonly?: boolean }) {
   const [initialData, setInitialData] = useState<any>();
-  const [isPublic, setIsPublic] = useState(props.drawing.public);
   const debouncedSave = useRef(
     _.debounce((elements, appState, files) => {
       const json = serializeAsJSON(elements, appState, files, "local");
@@ -63,41 +59,12 @@ export default function App(props: { drawing: Drawing; readonly?: boolean }) {
     <Excalidraw
       initialData={initialData}
       viewModeEnabled={props.readonly}
-      renderTopRightUI={() => {
-        if (props.readonly) {
-          return null;
-        }
-        return (
-          <LiveCollaborationTrigger
-            isCollaborating={isPublic}
-            onSelect={async () => {
-              await fetch(isPublic ? "/api/unshare" : "api/share", {
-                method: "POST",
-                headers: {
-                  "Content-Type": "application/json",
-                },
-                body: JSON.stringify({
-                  key: props.drawing.key,
-                }),
-              });
-
-              setIsPublic(!isPublic);
-            }}
-          />
-        );
-      }}
       onChange={props.readonly ? undefined : debouncedSave.current}
     >
-      {/* <WelcomeScreen /> */}
       <MainMenu>
         <MainMenu.DefaultItems.LoadScene />
         <MainMenu.DefaultItems.Export />
         <MainMenu.DefaultItems.SaveAsImage />
-        {isPublic && !props.readonly ? (
-          <MainMenu.ItemLink icon={<UsersIcon />} href="/public">
-            Open Public Link
-          </MainMenu.ItemLink>
-        ) : null}
         <MainMenu.DefaultItems.Help />
         <MainMenu.DefaultItems.ClearCanvas />
         <MainMenu.Separator />
